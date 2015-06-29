05ebad671b56993b140c241457ac4d144d0daa89
Jisan Zaman <jzthegreat@gmail.com>
2015-06-29 18:08:17 -0400

    added dispense method to pezdispenser

--- Example.java
+++ Example.java
@@ -4,5 +4,24 @@ public class Example{
 		PezDispenser dispenser = new PezDispenser("Yoda");
 		System.out.printf("The dispenser character is %s\n", 
 			dispenser.getCharacterName());
-	}
+	
+  
+    if(dispenser.isEmpty()){
+      System.out.println("It is currently empty");
+    }
+    System.out.println("Loading...");
+    dispenser.load();
+  
+    if(!dispenser.isEmpty()){
+      System.out.println("It is no longer empty");
+    };
+
+    while(dispenser.dispense()){
+    	System.out.println("CHOMP!");
+    }
+
+    if(dispenser.isEmpty()){
+    	System.out.println("Ate all the Pez!");
+    }
+  }
 }
